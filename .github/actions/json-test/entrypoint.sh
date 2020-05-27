197bbd038af3e415e6f0e0015e9e8cf0dad88971
Tobias Limmer <tobias.limmer@siemens.com>
2020-05-27 18:53:17 +0200

    insert validator script into action

--- .github/actions/json-test/entrypoint.sh
+++ .github/actions/json-test/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "Hello World"
-find
-echo "::set-output name=good"
+python3 /sandbox/csaf_2.0/json_schema/test/validator.py /sandbox/csaf_2.0/json_schema/csaf_json_schema.json /sandbox/csaf_2.0/json_schema/CVE-2018-0171-modified.json
+echo "::set-output result=good"
