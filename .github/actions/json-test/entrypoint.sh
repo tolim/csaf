aac0213670cfcaf0da48bc0b508e4935c7b24182
Tobias Limmer <tobias.limmer@siemens.com>
2020-05-27 18:57:35 +0200

    show file structure for debugging

--- .github/actions/json-test/entrypoint.sh
+++ .github/actions/json-test/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+find
 python3 /sandbox/csaf_2.0/json_schema/test/validator.py /sandbox/csaf_2.0/json_schema/csaf_json_schema.json /sandbox/csaf_2.0/json_schema/CVE-2018-0171-modified.json
 echo "::set-output result=good"
