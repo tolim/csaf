02ba90ff6ec2a031d932a984ac3193d878f51360
Tobias Limmer <tobias.limmer@siemens.com>
2020-05-27 21:44:19 +0200

    revised test script

--- .github/actions/json-test/entrypoint.sh
+++ .github/actions/json-test/entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+#echo "::set-output name=result::good"
 
-find
-pwd
-python3 sandbox/csaf_2.0/json_schema/test/validator.py sandbox/csaf_2.0/json_schema/csaf_json_schema.json sandbox/csaf_2.0/json_schema/CVE-2018-0171-modified.json
-echo "::set-output result=good"
+FAIL=0
+SCHEMA=sandbox/csaf_2.0/json_schema/csaf_json_schema.json 
+
+validate() {
+  echo -n "Testing file $1 against schema sandbox/csaf_2.0/json_schema/csaf_json_schema.json ... "
+  if python3 sandbox/csaf_2.0/json_schema/test/validator.py $SCHEMA $1; then
+    echo SUCCESS
+  else
+    echo FAILED
+    FAIL=1
+  fi
+
+}
+
+validate sandbox/csaf_2.0/json_schema/CVE-2018-0171-modified.json
+validate sandbox/csaf_2.0/json_schema/CVE-2018-0171-modified.json1
+
+exit $FAIL
